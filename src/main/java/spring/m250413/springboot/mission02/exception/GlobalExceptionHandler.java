f89a860b75955f61e6ce538f94ed7684c5da1c65
bo <bo9718@naver.com>
2025-04-20 21:22:47 +0900

    feat: 250420 spring boot mission02 front

--- src/main/java/spring/m250413/springboot/mission02/exception/GlobalExceptionHandler.java
+++ src/main/java/spring/m250413/springboot/mission02/exception/GlobalExceptionHandler.java
@@ -2,8 +2,11 @@ package spring.m250413.springboot.mission02.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -26,5 +29,33 @@ public class GlobalExceptionHandler {
                 e.getMessage()
         );
         return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(MethodArgumentNotValidException ex) {
+        System.out.println("✅ handleValidation 작동"); // 예외 핸들러 안에 로그 찍기
+        String message = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(","));
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                message
+        );
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAll(Exception e) {
+        System.out.println("❗ 예외 타입: " + e.getClass().getName());
+        e.printStackTrace(); // 콘솔에 전체 로그 찍기
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Unexpected Error",
+                e.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 }
